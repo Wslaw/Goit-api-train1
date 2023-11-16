95a873787dcea7d5bf8a856817702f890cafd00f
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-16 13:46:38 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-
 //                    API
-// https://rickandmortyapi.com/api/character
-https://rickandmortyapi.com/api/character
+const city = document.querySelector(".city");
+const day = document.querySelector(".day");
+const btn = document.querySelector(".btn");
+console.log(city);
+console.log(day);
+console.log(btn);
+
+const URL = "https://api.thecatapi.com/v1/images/0XYvRd7oD";
+
+fetch(URL)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("404 not found!");
+    }
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  .then(() => console.log("Finish"))
+  .catch((err) => console.log(`Fetch error: ${err}`));
